2472ddcc4176bcfb95ef467df5ee2b3cb9405200
jayAch <jhall_1974@hotmail.com>
2026-09-15 17:48:31 -0400

    update

--- exclude_list.py
+++ exclude_list.py
@@ -8,7 +8,15 @@ GLOBAL_EXCLUDE_LIST = [
 "б", "в", "г", "д", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "п", "т", "ф", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я", "Б", "В", "Г", "Д", "Ё", "Ж", "З", "И", "Й", "К", "Л", "М", "Н", "П", "Т", "Ф", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь", "Э", "Ю", "Я",
 
 # All other entries (alphabetized, case-sensitive, deduplicated)
+"Drone TV",
+"No Reservations",
+"Wicked Tuna",
+"Valuetainment",
+"Six Million Dollar Man",
+"Bionic Woman",
 "Mr.Bean",
+"Quirky AF",
+"Ghosts",
 "Leave it to Beaver",
 "According to Jim",
 "AFV with Alfonso Ribeiro",
