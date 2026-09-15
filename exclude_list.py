47106d048b98a2745b093a227e52a67ca435f36b
jayAch <jhall_1974@hotmail.com>
2026-09-15 15:00:56 -0400

    update

--- exclude_list.py
+++ exclude_list.py
@@ -8,6 +8,12 @@ GLOBAL_EXCLUDE_LIST = [
 "б", "в", "г", "д", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "п", "т", "ф", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я", "Б", "В", "Г", "Д", "Ё", "Ж", "З", "И", "Й", "К", "Л", "М", "Н", "П", "Т", "Ф", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь", "Э", "Ю", "Я",
 
 # All other entries (alphabetized, case-sensitive, deduplicated)
+"UEFA.tv",
+"Untold Stories of the E.R.",
+"WFMY Greensboro 2",
+"WXII Very Carolina 12",
+"Vice",
+"Localish",
 "Retro Black Laughs",
 "Today All Day",
 "Top Rank Classics",
