23fc9ee722fdabae318b4f58bd865a5fe6257f4d
jayAch <jhall_1974@hotmail.com>
2026-09-14 23:50:58 -0400

    update

--- exclude_list.py
+++ exclude_list.py
@@ -8,6 +8,13 @@ GLOBAL_EXCLUDE_LIST = [
 "б", "в", "г", "д", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "п", "т", "ф", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я", "Б", "В", "Г", "Д", "Ё", "Ж", "З", "И", "Й", "К", "Л", "М", "Н", "П", "Т", "Ф", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь", "Э", "Ю", "Я",
 
 # All other entries (alphabetized, case-sensitive, deduplicated)
+"The Lone Ranger",
+"The Virginian",
+"Et Ton Cash",
+"Temptation Island",
+"Divaernes Dillemmaer",
+"The Brady Bunch",
+"Highway To Heaven",
 "Touch By An Angel",
 "In the Heat of the Night",
 "In The Heat Of The Night",
