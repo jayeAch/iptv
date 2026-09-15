f23ba856569adb4d502bafc1a5d15018fe1821e6
jayAch <jhall_1974@hotmail.com>
2026-09-15 01:21:26 -0400

    update

--- exclude_list.py
+++ exclude_list.py
@@ -8,6 +8,8 @@ GLOBAL_EXCLUDE_LIST = [
 "б", "в", "г", "д", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "п", "т", "ф", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я", "Б", "В", "Г", "Д", "Ё", "Ж", "З", "И", "Й", "К", "Л", "М", "Н", "П", "Т", "Ф", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь", "Э", "Ю", "Я",
 
 # All other entries (alphabetized, case-sensitive, deduplicated)
+"FreeTV Estelar",
+"FreeTV Sureño",
 "Wapa+",
 "FilmRise Creators",
 "Watch AEW",
@@ -36,7 +38,6 @@ GLOBAL_EXCLUDE_LIST = [
 "Felicity",
 "Broad City",
 "Sam and Colby",
-"Alien Files Unsealed",
 "Kebab Date",
 "Danske Hollywoodfruer",
 "Amalies verden",
