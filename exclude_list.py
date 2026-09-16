da7c5445111311f4594806227e7113ddfc72002c
jayAch <jhall_1974@hotmail.com>
2026-09-16 18:57:26 -0400

    update

--- exclude_list.py
+++ exclude_list.py
@@ -8,7 +8,14 @@ GLOBAL_EXCLUDE_LIST = [
 "б", "в", "г", "д", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "п", "т", "ф", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я", "Б", "В", "Г", "Д", "Ё", "Ж", "З", "И", "Й", "К", "Л", "М", "Н", "П", "Т", "Ф", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь", "Э", "Ю", "Я",
 
 # All other entries (alphabetized, case-sensitive, deduplicated)
+"Charmed",
+"OUTflix Proud",
+"Logo Pluto TV",
+"Pluto TV Pride",
+"OUTFLIX MOVIES",
+"NatureStream.TV",
 "Trucking Hell",
+"Fairground AI Creator TV",
 "Emergency Rescue",
 "Pluto TV PetrolHeads",
 "Fight Network",
