5024667dad2b1823c90281e467ce089eff2e0d8d
jayAch <jhall_1974@hotmail.com>
2026-09-16 22:38:03 -0400

    update

--- exclude_list.py
+++ exclude_list.py
@@ -8,6 +8,11 @@ GLOBAL_EXCLUDE_LIST = [
 "б", "в", "г", "д", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "п", "т", "ф", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я", "Б", "В", "Г", "Д", "Ё", "Ж", "З", "И", "Й", "К", "Л", "М", "Н", "П", "Т", "Ф", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь", "Э", "Ю", "Я",
 
 # All other entries (alphabetized, case-sensitive, deduplicated)
+"FilmRise Classic TV",
+"Pluto TV Retro Crime Action",
+"Classic TV Drama",
+"Pluto TV Classic TV",
+"Pluto TV Classic TV Variety",
 "KGTV",
 "KGUN",
 "KING Seattle",
