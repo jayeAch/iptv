8a416ff7cad822d944010d6be5e9ed100cd0c9a6
jayAch <jhall_1974@hotmail.com>
2026-09-16 22:34:20 -0400

    update

--- exclude_list.py
+++ exclude_list.py
@@ -8,6 +8,12 @@ GLOBAL_EXCLUDE_LIST = [
 "б", "в", "г", "д", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "п", "т", "ф", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я", "Б", "В", "Г", "Д", "Ё", "Ж", "З", "И", "Й", "К", "Л", "М", "Н", "П", "Т", "Ф", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь", "Э", "Ю", "Я",
 
 # All other entries (alphabetized, case-sensitive, deduplicated)
+"KGTV",
+"KGUN",
+"KING Seattle",
+"KNXV",
+"KOAT",
+"KPNX",
 "FilmRise British TV",
 "Silent Witness and New Tricks",
 "Pluto TV Action Drama",
