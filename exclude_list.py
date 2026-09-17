9c654e175a1a271f2a71860e221fc982a390461f
jayAch <jhall_1974@hotmail.com>
2026-09-17 13:33:56 -0400

    update

--- exclude_list.py
+++ exclude_list.py
@@ -8,6 +8,12 @@ GLOBAL_EXCLUDE_LIST = [
 "б", "в", "г", "д", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "п", "т", "ф", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я", "Б", "В", "Г", "Д", "Ё", "Ж", "З", "И", "Й", "К", "Л", "М", "Н", "П", "Т", "Ф", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь", "Э", "Ю", "Я",
 
 # All other entries (alphabetized, case-sensitive, deduplicated)
+"Grjngo",
+"Circle",
+"get TV",
+"CW FOREVER",
+"Filmrise British TV",
+"Thailand TV",
 "BBC Drama",
 "ElectricNOW",
 "Classic TV Comedy",
@@ -1374,7 +1380,6 @@ GLOBAL_EXCLUDE_LIST = [
 "Smithsonian",
 "Smooth Jazz",
 "Smurf",
-"SNL Vault",
 "Snooker 900",
 "So Yummy",
 "So... Real",
